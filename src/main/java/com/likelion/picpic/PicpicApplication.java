c026529b536fa617a79865dd10a82a31347580ae
김교휘 <hwee000@naver.com>
2023-09-18 20:50:06 +0900

    first commit

---
+++ src/main/java/com/likelion/picpic/PicpicApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.picpic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/picpic/PicpicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.picpic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
