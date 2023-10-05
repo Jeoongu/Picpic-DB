5f1b44fc216f4adc0725c1b443fec8072d302b3e
Jeoongu <ms235280@naver.com>
2023-10-05 22:53:58 +0900

    add : S3 CRUD

---
+++ src/main/java/com/likelion/picpic/service/S3Service.java
@@ -0,0 +1,57 @@
+package com.likelion.picpic.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+
+    /*
+    getURl()을 통해 파일이 저장된 URL을 return 해주고,
+    이 URL로 이동 시 해당 파일이 오픈됨(버킷 정책 변경 완료)
+     */
+    public String saveFile(MultipartFile multipartFile) throws IOException {
+        String originalFilename = multipartFile.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
+        return amazonS3.getUrl(bucket, originalFilename).toString();
+    }
+
+    // 이미지 다운로드, 리턴값 변경 필요한지 찾아봐야함.
+    public ResponseEntity<UrlResource> downloadImage(String originalFilename) {
+        UrlResource urlResource = new UrlResource(amazonS3.getUrl(bucket, originalFilename));
+
+        String contentDisposition = "attachment; filename=\"" +  originalFilename + "\"";
+
+        // header에 CONTENT_DISPOSITION 설정을 통해 클릭 시 다운로드 진행
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource);
+
+    }
+
+    // 버킷에 올라간 파일 삭제
+    public void deleteImage(String originalFilename)  {
+        amazonS3.deleteObject(bucket, originalFilename);
+    }
+}
+
