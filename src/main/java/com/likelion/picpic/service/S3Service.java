ada546c88bbcb94821820f069d10dd40b45f105a
김교휘 <hwee000@naver.com>
2023-10-09 18:16:40 +0900

    login

--- src/main/java/com/likelion/picpic/service/S3Service.java
+++ src/main/java/com/likelion/picpic/service/S3Service.java
@@ -3,6 +3,8 @@ package com.likelion.picpic.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.likelion.picpic.domain.User;
+import com.likelion.picpic.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.UrlResource;
@@ -12,22 +14,38 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class S3Service {
     private final AmazonS3 amazonS3;
+    private final UserService userService;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+    @Value("&{jwt.secret}")
+    private String secretKey;
+
+    public Long getUserId(String token){
+        //토큰으로 userId가져오기
+        String userEmail=JwtUtil.getEmail(token, secretKey);
+        Long userId=userService.findUserId(userEmail);
+        //토큰으로 userId가져오기
+        return userId;
+    }
 
 
     /*
     getURl()을 통해 파일이 저장된 URL을 return 해주고,
     이 URL로 이동 시 해당 파일이 오픈됨(버킷 정책 변경 완료)
      */
-    public String saveFile(MultipartFile multipartFile) throws IOException {
-        String originalFilename = multipartFile.getOriginalFilename();
+    public String saveFile(String token, MultipartFile multipartFile) throws IOException {
+        Long userId=getUserId(token);
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date());
+        String originalFilename = userId+"/"+timeStamp+"_"+multipartFile.getOriginalFilename();
+
 
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(multipartFile.getSize());
