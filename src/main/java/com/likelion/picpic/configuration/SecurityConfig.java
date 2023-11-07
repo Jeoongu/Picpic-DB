436340cc459c2e3ce5e37936861c175f1a720d31
김교휘 <hwee000@naver.com>
2023-11-08 02:58:30 +0900

    config fixed

--- src/main/java/com/likelion/picpic/configuration/SecurityConfig.java
+++ src/main/java/com/likelion/picpic/configuration/SecurityConfig.java
@@ -2,46 +2,54 @@ package com.likelion.picpic.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.List;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeHttpRequests()
-//                .antMatchers("/**")
-//                //.requestMatchers(new AntPathRequestMatcher("/**"))
-//                .permitAll()
-//                .and()
-//                // CORS 설정
-//                .cors(c -> {
-//                    CorsConfigurationSource source = request -> {
-//                        // CORS 허용 패턴
-//                        CorsConfiguration config = new CorsConfiguration();
-//                        config.setAllowedOrigins(
-//                                List.of("*")
-//                        );
-//                        config.setAllowedMethods(
-//                                List.of("*")
-//                        );
-//                        config.setAllowedHeaders(
-//                                List.of("*")
-//                        );
-//
-//                        return config;
-//                    };
-//                    c.configurationSource(source);
-//                });
-//        return http.build();
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // CSRF 설정 비활성화
+                .csrf().disable()
+
+                // 모든 요청에 대해 접근 허용
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+
+                // CORS 설정
+                .and().cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 내부에서 설정한 경우
+        config.addAllowedOriginPattern("*"); // 모든 Origin 허용
+        config.addAllowedMethod("*"); // 모든 HTTP 메소드 허용
+        config.addAllowedHeader("*"); // 모든 HTTP 헤더 허용
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    // CORS 설정
+    private CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // JSESSIONID 등의 쿠키를 주고받기 위함
+        config.addAllowedOriginPattern("*"); // 모든 Origin 허용
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        config.addAllowedMethod("*"); // 모든 (HTTP) 메소드 허용
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
